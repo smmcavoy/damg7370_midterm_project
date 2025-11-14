b5f1ea71ec94b0eb82e045f395af1fc07f479bb8
smmcavoy <mcavoyseamus@gmail.com>
2025-11-14 04:37:53 +0000

    fkme

--- restaurant_pipeline/write_tables.py
+++ restaurant_pipeline/write_tables.py
@@ -4,9 +4,9 @@ for t in [
     'workspace.damg7370.dim_rpl_facility',
     'workspace.damg7370.dim_rpl_location',
     'workspace.damg7370.dim_rpl_violation',
-    'workspace.damg7370.dim_inspection_violation_bridge'
+    'workspace.damg7370.dim_rpl_inspection_violation_bridge'
 ]:
     df = spark.table(t)
-    output_path = f'/Volumes/workspace/damg7370/restaurant_data/output/{t.split(".")[-1]}.csv'
+    output_path = f'/Volumes/workspace/damg7370/restaurant_data/output3/{t.split(".")[-1]}'
     df.write.mode("overwrite").format("csv").option("header", "true").save(output_path)
 # Volumes paths are not supported for this operation; use /tmp/ for file output.
