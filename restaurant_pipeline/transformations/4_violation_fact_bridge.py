5b0cf9a18aebca0887fbd02e76ec13f8adbd65f2
smmcavoy <mcavoyseamus@gmail.com>
2025-11-14 04:31:25 +0000

    hi

--- restaurant_pipeline/transformations/4_violation_fact_bridge.py
+++ restaurant_pipeline/transformations/4_violation_fact_bridge.py
@@ -1,13 +1,18 @@
 from pyspark import pipelines as dp
 import pyspark.sql.functions as sf
 
+@dp.table()
 def dim_rpl_inspection_violation_bridge():
     df = spark.sql(
         """
         select inspection_key, explode(clean_violations) as violation_description
         from pl4_silver_combined
         """
-    ).distinct().join('dim_rpl_violation', on='violation_description')
+    ).distinct().join(
+        spark.table('dim_rpl_violation'),
+        on="violation_description",
+        how="left"
+    )
     return df.drop('violation_description')
 
 
